197dc2bbe7d14dcea6f67ddeacfaa9f4dc0ff791
Rob Shannon <spirit2457@gmail.com>
2013-09-15 13:46:27 +0100

    Update common_full.mk

--- config/common_full.mk
+++ config/common_full.mk
@@ -12,30 +12,4 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     VisualizationWallpapers \
     PhaseBeam \
-		Torch \
-		LockClock \
-		ShareLogs \
-		PerformanceTool \
-		Email \
-		busybox
-
-# Openssh
-PRODUCT_PACKAGES += \
-    scp \
-    sftp \
-    ssh \
-    sshd \
-    sshd_config \
-    ssh-keygen \
-		rsync
-
-
-# T-Mobile theme engine
-PRODUCT_PACKAGES += \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
-
-PRODUCT_COPY_FILES += \
-       vendor/ax/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
-
+    ShareLogs
