bd28bc19492ba478d12026a5bc6c3b6988bfc620
Rob <spirit2457@gmail.com>
2015-07-21 12:12:19 +0100

    Update the pure-aosp vendor from omni's this also includes the sepolicy fixes for asec mounting

--- config/common.mk
+++ config/common.mk
@@ -1,76 +1,38 @@
 PRODUCT_BRAND ?= aosp
 
-ifdef pap_NIGHTLY
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=aospN
+    ro.com.google.clientidbase=android-google
 else
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=aosp+
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
+# general properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    keyguard.no_require_sim=true \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
-    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
-    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    persist.sys.root_access=1
 
-# Enable SIP+VoIP on all targets
+# enable ADB authentication if not on eng build
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+endif
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/pap/prebuilt/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/pap/prebuilt/bin/sysinit:system/bin/sysinit
+
+# userinit support
+PRODUCT_COPY_FILES += \
+    vendor/pap/prebuilt/etc/init.d/90userinit:system/etc/init.d/90userinit
+
+# Init script file with pap extras
+PRODUCT_COPY_FILES += \
+    vendor/pap/prebuilt/etc/init.local.rc:root/init.pap.rc
+
+# Enable SIP and VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/pap/overlay/dictionaries
-PRODUCT_PACKAGE_OVERLAYS += vendor/pap/overlay/common
-
-PRODUCT_VERSION_MAJOR = 1
-PRODUCT_VERSION_MINOR = 0
-PRODUCT_VERSION_MAINTENANCE = BETA
-
-# Set PAP_BUILDTYPE
-ifdef PAP_NIGHTLY
-    PAP_BUILDTYPE := NIGHTLY
-endif
-ifdef PAP_EXPERIMENTAL
-    PAP_BUILDTYPE := EXPERIMENTAL
-endif
-ifdef PAP_RELEASE
-    PAP_BUILDTYPE := RELEASE
-endif
-
-ifdef PAP_BUILDTYPE
-    ifdef PAP_EXTRAVERSION
-        # Force build type to EXPERIMENTAL
-        PAP_BUILDTYPE := EXPERIMENTAL
-        # Add leading dash to PAP_EXTRAVERSION
-        PAP_EXTRAVERSION := -$(PAP_EXTRAVERSION)
-    endif
-else
-    # If pap_BUILDTYPE is not defined, set to UNOFFICIAL
-    PAP_BUILDTYPE := UNOFFICIAL
-    PAP_EXTRAVERSION :=
-endif
-
-ifdef PAP_RELEASE
-    PAP_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(PRODUCT_VERSION_MAINTENANCE)$(PRODUCT_VERSION_DEVICE_SPECIFIC)-$(PAP_BUILD)
-else
-    ifeq ($(PRODUCT_VERSION_MINOR),0)
-        PAP_VERSION := $(PRODUCT_VERSION_MAJOR)-$(shell date -u +%Y%m%d)-$(PAP_BUILDTYPE)-$(pap_BUILD)$PAP_EXTRAVERSION)
-    else
-        PAP_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(PAP_BUILDTYPE)-$(PAP_BUILD)$(PAP_EXTRAVERSION)
-    endif
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.pap.version=$(PAP_VERSION) \
-  ro.modversion=pap-$(PAP_VERSION) \
-  ro.goo.developerid=Lloir \
-  ro.goo.rom=PureAosp \
-  ro.goo.version=$(shell date +%s)
-
-
--include $(WORKSPACE)/hudson/image-auto-bits.mk
---
+++ config/packages.mk
@@ -0,0 +1,31 @@
+# Additional packages
+PRODUCT_PACKAGES += \
+    Basic \
+    Development \
+    LatinIME \
+    SoundRecorder \
+    VideoEditor \
+    VoiceDialer
+
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver
+
+# Additional tools
+PRODUCT_PACKAGES += \
+    bash \
+    e2fsck \
+    fsck.exfat \
+    htop \
+    lsof \
+    mke2fs \
+    mkfs.exfat \
+    mount.exfat \
+    nano \
+    openvpn \
+    powertop \
+    tune2fs \
+    vim \
+    ntfsfix \
+    ntfs-3g \
+    mkntfs
+
